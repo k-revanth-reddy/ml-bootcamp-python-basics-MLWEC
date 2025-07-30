c0e3201bf3632f5a66e10fd7b9c0c04bf524c7a3
Enter you github user name <Enter your email>
2025-07-30 13:45:05 +0530

    uploading python file

---
+++ app1.py
@@ -0,0 +1,16 @@
+name="MLWEC"
+number=10.0
+isOn=True
+letter="H"
+# Arithemetic- + - * / %
+#logical - and,or,not
+#comparison- <,>,==,!=,<=,>=
+#assignment - =,+=,-=,*=,/=
+if (number>5):
+    print(name)
+else:
+    print(isOn)
+
+
+
+    
